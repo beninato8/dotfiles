fdeaf8cb0307de221f8e105a502c614f43edf02a
Nicholas Beninato <beninato8@gmail.com>
2022-05-23 04:03:28 -0500

    oh my zsh

---
+++ .oh-my-zsh-custom/better-completion.zsh
@@ -0,0 +1,4 @@
+zstyle ':completion:*' completer _complete
+# zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'm:{a-zA-Z}={A-Za-z} r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+autoload -Uz compinit
+compinit
