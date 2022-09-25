ff6b9367fc866c70af9116dfdea1a006d938e479
Nicholas Beninato <beninato8@gmail.com>
2022-09-24 22:38:40 -0500

    update commits

--- bin/commits.py
+++ bin/commits.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/local/bin/python3
 
 import requests
 from datetime import datetime
 from bs4 import BeautifulSoup as bs4
 try:
     txt = requests.get('https://github.com/beninato8').text
-    # print(txt)
     d = datetime.now().strftime('%Y-%m-%d')
-    b = bs4(txt, 'html.parser').find('rect', {'class':'day', 'data-date':d})['data-count']
+    # print(bs4(txt, 'html.parser').find('rect'))
+    b = bs4(txt, 'html.parser').find('rect', {'class':'ContributionCalendar-day', 'data-date':d})['data-count']
     print(b)
 except Exception as e:
     pass
