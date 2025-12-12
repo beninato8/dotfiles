f2732f128fc746d2f6a3a4715ae4ee24d235ee3b
Nicholas Beninato <beninato8@users.noreply.github.com>
2025-12-12 01:10:29 -0800

    commits

--- bin/commits.py
+++ bin/commits.py
@@ -1,13 +1,39 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
+import os
 import requests
 from datetime import datetime
-from bs4 import BeautifulSoup as bs4
-try:
-    txt = requests.get('https://github.com/beninato8').text
-    d = datetime.now().strftime('%Y-%m-%d')
-    # print(bs4(txt, 'html.parser').find('rect'))
-    b = bs4(txt, 'html.parser').find('td', {'class':'ContributionCalendar-day', 'data-date':d})['data-level']
-    print(b)
-except Exception as e:
-    pass
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("GITHUB_TOKEN")
+if not TOKEN:
+    raise ValueError("GitHub token not found. Set it in a .env file.")
+
+GITHUB_USERNAME = "beninato8"
+
+query = """
+{
+  user(login: "%s") {
+    contributionsCollection(from: "%sT00:00:00Z", to: "%sT23:59:59Z") {
+      contributionCalendar {
+        totalContributions
+      }
+    }
+  }
+}
+""" % (GITHUB_USERNAME, datetime.utcnow().date(), datetime.utcnow().date())
+
+url = "https://api.github.com/graphql"
+headers = {"Authorization": f"bearer {TOKEN}"}
+
+response = requests.post(url, json={"query": query}, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    contributions = data["data"]["user"]["contributionsCollection"]["contributionCalendar"]["totalContributions"]
+    print(contributions)
+else:
+    print("❌")
